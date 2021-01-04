bfc3a5a364f29dd60036e0720f5773f4bb95e0ef
TheFinn15 <timlavr87@gmail.com>
2021-01-05 01:57:08 +0200

    add auth to frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -33,14 +33,43 @@
       <v-spacer/>
       <v-menu transition="fade-transition" offset-y>
         <template v-slot:activator="{on, attrs}">
-          <v-avatar color="#1E88E5" v-bind="attrs" v-on="on">
+          <v-avatar color="#1E88E5" v-bind="attrs" v-on="on" title="Anonymous">
             A
           </v-avatar>
         </template>
         <v-card>
           <v-card-subtitle>
-            KEs
+            Не найдено
           </v-card-subtitle>
+          <v-divider/>
+          <v-list nav>
+            <v-list-item-group>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>
+                    person_pin
+                  </v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title @click="authForm.state = true">
+                    Авторизация
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>
+                    person_add
+                  </v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title @click="registerForm.state = true">
+                    Регистрация
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
         </v-card>
       </v-menu>
     </v-app-bar>
@@ -50,21 +79,57 @@
           transition="fade-transition"
           top
           timeout="1500"
-          v-model="chatAlert"
+          v-model="goToErr"
+          color="indigo"
       >
-        <span>Данная функция времена недоступна.</span>
+        <span>Войдите в свой кабинет для начала.</span>
       </v-snackbar>
-      <v-dialog>
-        <template v-slot:activator>
-
-        </template>
+      <v-snackbar
+          transition="fade-transition"
+          top
+          timeout="1500"
+          v-model="authForm.alertState"
+          :color="authForm.alertInfo[1]"
+      >
+        {{authForm.alertInfo[0]}}
+      </v-snackbar>
+      <v-dialog v-model="authForm.state" width="480">
+        <v-card>
+          <v-card-title style="justify-content: center; display: flex">
+            Авторизация
+          </v-card-title>
+          <v-divider/>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                    label="Логин"
+                    outlined
+                    v-model="authForm.login"
+                />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                    type="password"
+                    label="Пароль"
+                    outlined
+                    v-model="authForm.pwd"
+                />
+              </v-col>
+            </v-row>
+            <v-btn block color="success" @click="doAuth">
+              ВОЙТИ
+            </v-btn>
+          </v-container>
+        </v-card>
       </v-dialog>
-      <v-dialog></v-dialog>
       <router-view/>
     </v-main>
 
     <footer>
-      <v-bottom-navigation v-if="!isAuth" grow :value="bottomNav" color="#448AFF">
+      <v-bottom-navigation :value="bottomNav" grow color="#448AFF">
         <v-btn to="/">
           <span>
             Лайф-лента
@@ -73,7 +138,7 @@
             list_alt
           </v-icon>
         </v-btn>
-        <v-btn to="/cabinet">
+        <v-btn @click="goToCabinet">
           <span>
             Мой кабинет
           </span>
@@ -81,41 +146,12 @@
             account_box
           </v-icon>
         </v-btn>
-        <v-btn @click="() => {
-          chatAlert = !chatAlert
-          bottomNav = !bottomNav
-        }">
+        <v-btn disabled>
           <span>
             Мои чаты
           </span>
           <v-icon>
             chat
-          </v-icon>
-        </v-btn>
-      </v-bottom-navigation>
-      <v-bottom-navigation grow :value="bottomNav" color="#448AFF" v-else>
-        <v-btn to="/">
-          <span>
-            Лайф-лента
-          </span>
-          <v-icon>
-            list_alt
-          </v-icon>
-        </v-btn>
-        <v-btn>
-            <span>
-              Авторизация
-            </span>
-          <v-icon>
-            person_pin
-          </v-icon>
-        </v-btn>
-        <v-btn>
-            <span>
-              Регистрация
-            </span>
-          <v-icon>
-            person_add
           </v-icon>
         </v-btn>
       </v-bottom-navigation>
@@ -137,15 +173,55 @@ export default Vue.extend({
   name: "App",
   components: {},
   data: () => ({
-    drawer: false,
     createShortink: false,
     bottomNav: 0,
-    chatAlert: false
-  }),
-  computed: {
-    isAuth() {
-      return localStorage['uid'] !== undefined;
+    goToErr: false,
+    authForm: {
+      state: false,
+      login: null,
+      pwd: null,
+      alertState: false,
+      alertInfo: ['', 'success']
+    },
+    registerForm: {
+      state: false,
+      fname: null,
+      sname: null,
+      email: null,
+      login: null,
+      pwd: null,
+      alertState: false,
+      alertInfo: ['', 'success']
     }
+  }),
+  methods: {
+    goToCabinet() {
+      if (localStorage['uid'] !== undefined) {
+        this.$router.push('/cabinet')
+      } else {
+        this.$data.goToErr = true
+      }
+    },
+    doAuth() {
+      this.$store.commit('auth', {
+        login: this.$data.authForm.login,
+        pwd: this.$data.authForm.pwd
+      })
+      if (localStorage['uid'] === undefined) {
+        this.$data.authForm.alertState = true
+        this.$data.authForm.alertInfo[0] = 'Ошибка при авторизации'
+        this.$data.authForm.alertInfo[1] = 'red'
+      } else {
+        this.$data.authForm.alertState = true
+        this.$data.authForm.alertInfo[0] = 'Успешнная авторизация'
+        this.$data.authForm.alertInfo[1] = 'success'
+      }
+    }
+  },
+  mounted() {
+    if (this.$route.path === '/') this.$data.bottomNav = 0
+    if (this.$route.path === '/cabinet') this.$data.bottomNav = 1
+    if (this.$route.path === '/chats') this.$data.bottomNav = 2
   }
 });
 </script>
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,14 +1,28 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    count: 0
+    token: ''
   },
   mutations: {
-    increase: state => state.count++
+    auth(state: any, userInfo: any) {
+      axios({
+        method: 'POST',
+        url: `http://localhost:25016/api/auth`,
+        data: {
+          login: userInfo.login,
+          pwd: userInfo.pwd
+        }
+      }).then(resp => {
+        localStorage['uid'] = resp.data['id_token']
+      }).catch(() => {
+        state.isLogin = ''
+      })
+    }
   },
   actions: {},
   modules: {}
