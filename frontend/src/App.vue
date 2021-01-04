ebefb416278e30137220385f18b78d257a96b146
TheFinn15 <timlavr87@gmail.com>
2021-01-04 02:43:13 +0200

    rebuild frontend to vue.js

---
+++ frontend/src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-app>
+    <v-app-bar app color="#448AFF" dark>
+      <v-icon x-large>
+        link
+      </v-icon>
+      <v-card-title>
+        <router-link to="/" style="color: inherit; text-decoration: none">
+          Shortink
+        </router-link>
+      </v-card-title>
+      <v-btn icon>
+        <v-icon>
+          translate
+        </v-icon>
+      </v-btn>
+      <v-dialog>
+        <template v-slot:activator="{on, attrs}">
+          <v-btn icon
+                 title="Создать сокращенную ссылку"
+                 v-on="on" v-bind="attrs">
+            <v-icon>
+              create
+            </v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            Новая ссылка
+          </v-card-title>
+        </v-card>
+      </v-dialog>
+      <v-spacer/>
+      <v-menu transition="fade-transition" offset-y>
+        <template v-slot:activator="{on, attrs}">
+          <v-avatar color="#1E88E5" v-bind="attrs" v-on="on">
+            A
+          </v-avatar>
+        </template>
+        <v-card>
+          <v-card-subtitle>
+            KEs
+          </v-card-subtitle>
+        </v-card>
+      </v-menu>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+
+    <footer>
+      <v-bottom-navigation grow :value="bottomNav" color="#448AFF">
+        <v-btn to="/">
+          <span>
+            Лайф-лента
+          </span>
+          <v-icon>
+            list_alt
+          </v-icon>
+        </v-btn>
+        <v-btn to="/cabinet">
+          <span>
+            Мой кабинет
+          </span>
+          <v-icon>
+            account_box
+          </v-icon>
+        </v-btn>
+        <v-btn @click="() => {
+          chatAlert = !chatAlert
+          bottomNav = !bottomNav
+        }">
+          <span>
+            Мои чаты
+          </span>
+          <v-icon>
+            chat
+          </v-icon>
+        </v-btn>
+      </v-bottom-navigation>
+      <v-snackbar
+          transition="fade-transition"
+          top
+          timeout="1500"
+          v-model="chatAlert"
+      >
+        <span>Данная функция времена недоступна.</span>
+      </v-snackbar>
+    </footer>
+  </v-app>
+</template>
+
+<script lang="ts">
+// TODO: Сокращатель ссылок:
+// TODO: Полное управление ссылками и предпросмотр;
+// TODO: Мульти ссылки(скрещивание ссылок в одну, с возможностью перехода на каждую из них;
+// TODO: Лайф-лента ссылок;
+// TODO: Метод шифровки для ссылок и также мульти-ссылок.
+// TODO: Просмотр профилей других юзеров
+// TODO: ЧАТ ПОЛЬЗОВАТЕЛЕЙ (не точно)
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+  components: {},
+  data: () => ({
+    drawer: false,
+    createShortink: false,
+    bottomNav: 0,
+    chatAlert: false
+  })
+});
+</script>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-card style="margin: 5% 15% 0 15%">
+    <v-container>
+      <v-row>
+        <v-col sm="12" md="6" lg="4" xl v-for="i in 5" :key="i">
+          <v-card>
+            <v-toolbar flat>
+              <router-link :to="'/profile/user_'+i" style="color: inherit; text-decoration: none">
+                <v-toolbar-title>
+                  <v-avatar color="#1E88E5">
+                    <span style="color: white">
+                      {{i}}
+                    </span>
+                  </v-avatar>
+                  User_{{i}}
+                </v-toolbar-title>
+              </router-link>
+              <v-spacer/>
+              <v-btn icon>
+                <v-icon>
+                  more_vert
+                </v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-divider/>
+            <v-card-text>
+              dsadadadd
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  data: () => ({
+    list: []
+  }),
+  components: {},
+  methods: {
+    getLiveList() {
+      for (let i=0;i<5;i++) {
+        this.$data.list.push(i)
+      }
+    }
+  }
+})
+export default class Home extends Vue {}
+</script>
